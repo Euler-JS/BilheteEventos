9a633871cdcc82f5f93cd3d7ae4f2f863da5de0a
Euler-JS <joaoalbertojose96@gmail.com>
2022-09-13 10:33:20 +0200

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  eventosRegistrados=[
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento1.jpg"
+    },
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento7.jpg"
+    },
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento2.jpg"
+    },
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento3.jpg"
+    },
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento4.jpg"
+    },
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento5.jpg"
+    },
+    {
+      "titulo":"Julia Lucas & Hernani da Silva",
+      "local": "Local: Maputo",
+      "detalhes":"Um grande Show que os ... esperam",
+      "imagem":"../../assets/img/evento6.jpg"
+    }
+  ]
+  constructor() {}
+
+}
